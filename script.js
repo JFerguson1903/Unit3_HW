f4e69d1551e96a64b54f570f91f0ea4a744fa91e
Jessica Ferguson <jferguson1903@gmail.com>
2020-11-15 12:39:00 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+
+    var passwordLength = prompt("Enter password length (8 to 128 characters)");
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert("not within the approved range");
+        return;
+    } else if (isNaN(passwordLength)) {
+        alert("Not a valid entry, please try again.");
+        return;
+    }
+
+    console.log(passwordLength);
+
+    var passwordLowercase = confirm("Include lowercase characters");
+    console.log(passwordLowercase);
+    var passwordUppercase = confirm("Include uppercase characters");
+    console.log(passwordUppercase);
+    var passwordNumeric = confirm("Include numeric characters");
+    console.log(passwordNumeric);
+    var passwordSpecial = confirm("Include special characters");
+    console.log(passwordSpecial);
+
+    if (passwordLowercase === false && passwordUppercase === false && passwordNumeric === false && passwordSpecial === false) {
+        alert("No character types were selected, please try again.");
+        return;
+    }
+
+    var passwordSelections = {
+        length: passwordLength,
+        lowercase: passwordLowercase,
+        uppercase: passwordUppercase,
+        numeric: passwordNumeric,
+        special: passwordSpecial
+    };
+
+    var passwordCharacters = "A length of " + passwordLength + " characters\n"
+
+    if (passwordSelections.lowercase) {
+        passwordCharacters += "lowercase characters\n"
+    }
+    if (passwordSelections.uppercase) {
+        passwordCharacters += "uppercase characters\n"
+    }
+    if (passwordSelections.numeric) {
+        passwordCharacters += "numeric characters\n"
+    }
+    if (passwordSelections.special) {
+        passwordCharacters += "special characters\n"
+    }
+
+
+    console.log(passwordSelections);
+
+    var verifyPassword = confirm("The password needs to include:\n" + passwordCharacters);
+    console.log(verifyPassword);
+    if (verifyPassword) {
+        var password = generatePassword();
+        var passwordText = document.querySelector("#password");
+
+        // confirm critera selected is correct //
+        passwordText.value = password;
+    } else {
+        alert("No password will be generated, please try again.");
+        return;
+    }
+
+}
+
+function generatePassword() {
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
